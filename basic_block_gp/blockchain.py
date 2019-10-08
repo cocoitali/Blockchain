f41bd226ec5071f7e2dab0a1b4589f5de4bab230
cocoitali <cmburatto@gmail.com>
2019-10-07 21:56:49 -0600

    refactored proof_of_work, blockchain.py

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -83,16 +83,17 @@ class Blockchain(object):
     def last_block(self):
         return self.chain[-1]
 
-    def proof_of_work(self,last_proof):
+    def proof_of_work(self):
         """
         Simple Proof of Work Algorithm
         - Find a number p' such that hash(pp') contains 6 leading
         zeroes, where p is the previous p'
         - p is the previous proof, and p' is the new proof
         """
+        block_string = json.dumps(self.last_block, sort_keys=True)
         proof = 0
         # for block 1, hash(1, p) = 000000x
-        while self.valid_proof(last_proof, proof) is False :
+        while self.valid_proof(block_string, proof) is False :
             proof += 1
 
         return proof

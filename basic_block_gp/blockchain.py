d970af56f205ff959cdecb91a8365f1432b57f0c
cocoitali <cmburatto@gmail.com>
2019-10-07 20:52:02 -0600

    clean up /chain

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -191,7 +191,6 @@ def new_transaction():
 @app.route('/chain', methods=['GET'])
 def last_proof():
     response = {
-        # TODO: Return the chain and its current length
         'currentChain' : blockchain.chain,
         'length' : len(blockchain.chain)
     }
